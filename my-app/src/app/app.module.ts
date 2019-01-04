51da64fe2819bb47b2ade4372d4a6a24242f10e7
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-01-03 21:37:23 -0600

    angular

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarsComponent,
+    CarComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
